5d80cee757122cd02d9939d9d66c415b38db7929
teddy <eloke990@gmail.com>
2026-06-22 23:20:53 +0100

    nothing special

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,11 +3,11 @@ function About(){
     <>
       <div className="about" id="about">
         <div className="about1">
-          <p>About Us</p>
-          <h1>Excellence in <br />Academic Writing.</h1>
+          <p className="reveal delay-2">About Us</p>
+          <h1 className="reveal delay-3">Excellence in <br />Academic Writing.</h1>
         </div>
         <div className="about2">
-          <p className="reveal delay-3">
+          <p className="reveal delay-4">
             We are a trusted academic writing agency dedicated to helping students, researchers and professionals.
             Our mission is to deliver high-quality and well researched content that meets academic standards while helping our clients reach academic goals. We are committed to excellence, confidentiality, and timely deilivery in every project we undertake.
             <br /><br />
